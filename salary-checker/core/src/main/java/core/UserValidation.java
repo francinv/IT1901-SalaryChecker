7c8464157d3ba5e81f5bb9afbd4be6485fb598fd
Seran Shanmugathas <Seran@live.no>
2021-09-19 18:13:07 +0200

    Made som changes in User and created UserValidation

---
+++ salary-checker/core/src/main/java/core/UserValidation.java
@@ -0,0 +1,55 @@
+package core;
+
+import java.util.regex.Pattern;
+
+public class UserValidation {
+    
+    private final Pattern FIRSTNAME_REGEX = Pattern.compile("^([a-åA-Å]{2,})$");
+    private final Pattern LASTNAME_REGEX = Pattern.compile("^([a-åA-Å]{2,})$");
+    private final Pattern PASSWORD_REGEX = Pattern.compile("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$");
+    private final Pattern SOCIAL_NUMBER_REGEX = Pattern.compile("^(0[1-9]|[1-2][0-9]|31(?!(?:0[2469]|11))|30(?!02))(0[1-9]|1[0-2])\\d{7}$");
+    private final Pattern EMPLOYEE_NUMBER_REGEX = Pattern.compile(""); //TODO
+    private final Pattern EMAIL_REGEX = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
+    private final Pattern TAX_COUNT_REGEX = Pattern.compile(""); //TODO
+    
+
+    private boolean checkValidRegex(Pattern regex, String string) {
+        return regex.matcher(string).matches();
+    }
+
+    public boolean isValidFirstname(String firstname) {
+        return checkValidRegex(FIRSTNAME_REGEX, firstname);
+    }
+
+    public boolean isValidLastname(String lastname) {
+        return checkValidRegex(LASTNAME_REGEX, lastname);
+    }
+
+    public boolean isValidPassword(String password) {
+        return checkValidRegex(PASSWORD_REGEX, password);
+    }
+
+    public boolean isValidSocialNumber(Long socialNumber) {
+        return checkValidRegex(SOCIAL_NUMBER_REGEX, String.valueOf(socialNumber));
+    }
+
+    public boolean isValidEmployeeNumber(int employeeNumber) {
+        return checkValidRegex(EMPLOYEE_NUMBER_REGEX, String.valueOf(employeeNumber));
+    }
+
+    public boolean isValidEmail(String email) {
+        return checkValidRegex(EMAIL_REGEX, email);
+    }
+    
+    public boolean isValidTaxCount(double taxCount) {
+        return checkValidRegex(TAX_COUNT_REGEX, String.valueOf(taxCount));
+    }
+
+    public boolean isValidUser(String firstname, String lastname, String email, String password,
+        long socialNumber, int employeeNumber, String employerEmail, double taxCount) {
+            return isValidFirstname(firstname) && isValidLastname(lastname) &&
+                   isValidEmail(email) && isValidPassword(password) && isValidSocialNumber(socialNumber) &&
+                   isValidEmployeeNumber(employeeNumber) && isValidEmail(employerEmail) && 
+                   isValidTaxCount(taxCount); 
+    }
+}
---
+++ salary-checker/core/src/test/java/core/UserTest.java
@@ -0,0 +1,48 @@
+package core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+
+    private User testUser;
+
+    @BeforeEach
+    public void setUp() {
+        testUser = new User("Firstname", "Lastname", "email@email.com", "password!123", 
+            22019893456L, 33333, "employer_email@email.com", 35.5);
+    }
+
+    @Test
+    void testGetMethods() {
+        Assertions.assertEquals("Firstname", testUser.getFirstname());
+        Assertions.assertEquals("Lastname", testUser.getLastname());
+        Assertions.assertEquals("email@email.com", testUser.getEmail());
+        Assertions.assertEquals("password!123", testUser.getPassword());
+        Assertions.assertEquals(22019893456L, testUser.getSocialNumber());
+        Assertions.assertEquals(33333, testUser.getEmployeeNumber());
+        Assertions.assertEquals("employer_email@email.com", testUser.getEmployerEmail());
+        Assertions.assertEquals(35.5, testUser.getTaxCount());
+    }
+
+    @Test
+    void testSetMethods() {
+        testUser.setFirstname("Sander");
+        Assertions.assertNotEquals("Firstname", testUser.getFirstname());
+        testUser.setLastname("Olsen");
+        Assertions.assertNotEquals("Lastname", testUser.getLastname());
+        testUser.setEmail("email@gmail.com");
+        Assertions.assertNotEquals("email@email.com", testUser.getEmail());
+        testUser.setPassword("passord");
+        Assertions.assertNotEquals("password!123", testUser.getPassword());
+        testUser.setSoscialNumber(22030199999L);
+        Assertions.assertNotEquals(22019893456L, testUser.getSocialNumber());
+        testUser.setEmployeeNumber(11111);
+        Assertions.assertNotEquals(33333, testUser.getEmployeeNumber());
+        testUser.setEmployerEmail("employer_email@gmail.com");
+        Assertions.assertNotEquals("employer_email@email.com", testUser.getEmployerEmail());
+        testUser.setTaxCount(34.5);
+        Assertions.assertNotEquals(35.5, testUser.getTaxCount());
+    }
+}
