b20dd3b8c06e5f9ecb125f365c47611d634ed7cd
Seran Shanmugathas <serans@stud.ntnu.no>
2021-09-29 20:35:27 +0000

    Added regex for tax count and employee number

--- salary-checker/core/src/main/java/salarychecker/core/UserValidation.java
+++ salary-checker/core/src/main/java/salarychecker/core/UserValidation.java
@@ -8,9 +8,9 @@ public class UserValidation {
     private final Pattern LASTNAME_REGEX = Pattern.compile("^([a-åA-Å]{2,})$");
     private final Pattern PASSWORD_REGEX = Pattern.compile("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$");
     private final Pattern SOCIAL_NUMBER_REGEX = Pattern.compile("^(0[1-9]|[1-2][0-9]|31(?!(?:0[2469]|11))|30(?!02))(0[1-9]|1[0-2])\\d{7}$");
-    private final Pattern EMPLOYEE_NUMBER_REGEX = Pattern.compile(""); //TODO
+    private final Pattern EMPLOYEE_NUMBER_REGEX = Pattern.compile("^([0-9]{5})$"); 
     private final Pattern EMAIL_REGEX = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
-    private final Pattern TAX_COUNT_REGEX = Pattern.compile(""); //TODO
+    private final Pattern TAX_COUNT_REGEX = Pattern.compile("^([0-9]{0,3})$");
     
 
     private boolean checkValidRegex(Pattern regex, String string) {
