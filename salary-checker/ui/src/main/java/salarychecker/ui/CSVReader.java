3acdcf0a9b0dd9b2ddcc08b5144bfcc634ea29a4
Seran Shanmugathas <Seran@live.no>
2021-10-08 17:44:28 +0200

    Created enum

--- salary-checker/ui/src/main/java/salarychecker/ui/CSVReader.java
+++ salary-checker/ui/src/main/java/salarychecker/ui/CSVReader.java
@@ -37,22 +37,23 @@ public class CSVReader {
                 }
 
             }
-/*
-* - SalgsID (A-colon)
-- AnleggStatus (O-colon)
-- Salgstype (P-colon)
-- Bekreftet (W-colon)
-- Kampanje (Z-colon)
-- Bruddgebyr(KR) (AI-colon)
-- Norsk Vannkraft (AJ-colon)
-- ProductHubNavn (AL-colon)
-* */
+
         }
         catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
 
+    /** 
+     * SalgsID (A-colon)
+     * AnleggStatus (O-colon)
+     * Salgstype (P-colon)
+     * Bekreftet (W-colon)
+     * Kampanje (Z-colon)
+     * Bruddgebyr(KR) (AI-colon)
+     * Norsk Vannkraft (AJ-colon)
+     * ProductHubNavn (AL-colon)
+     */
     public static void printCSV(){
         for (Record r: dataList) {
             System.out.println(r.toString());
