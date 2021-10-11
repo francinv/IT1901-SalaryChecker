0d217d3468ff794c03c8eadf4099378b35f4edd1
francinv <francinv@stud.ntnu.no>
2021-10-11 11:18:45 +0200

    (#24) Updated module-info with new requirements

--- salary-checker/core/src/main/java/module-info.java
+++ salary-checker/core/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module salarychecker.core {
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
     requires javax.mail;
+    requires com.opencsv;
+    requires java.sql;
     exports salarychecker.core;
     exports salarychecker.json;
     opens salarychecker.core;
