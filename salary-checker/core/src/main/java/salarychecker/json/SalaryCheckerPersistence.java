c12f370c8138dd2ed1b89b64a7a078e127481e34
Seran Shanmugathas <Seran@live.no>
2021-10-11 17:21:09 +0200

    (#36) Added encryption and decryption

--- salary-checker/core/src/main/java/salarychecker/json/SalaryCheckerPersistence.java
+++ salary-checker/core/src/main/java/salarychecker/json/SalaryCheckerPersistence.java
@@ -97,4 +97,33 @@ public class SalaryCheckerPersistence {
     return mapper.readValue(reader, Accounts.class);
   }
 
+
+  public static void main(String[] args) {
+
+    User user;
+    AdminUser adminUser;
+    Accounts accounts = new Accounts();
+  
+    user = new User("Seran", "Shanmugathas", "seran@live.no", "Volla!123", 
+      "26080191390", 55555, "francin@gmail.com", 33.3);
+    adminUser = new AdminUser("Francin", "Vincent", "francin@gmail.com", "Vandre!123");
+    accounts.addUser(user);
+    accounts.addUser(adminUser);
+  
+    
+    SalaryCheckerPersistence salaryCheckerPersistence = new SalaryCheckerPersistence();
+    try {
+      salaryCheckerPersistence.setSaveFile("Accounts.json");
+      salaryCheckerPersistence.saveAccounts(accounts);
+      Accounts accounts2 = salaryCheckerPersistence.loadAccounts();
+      System.out.println(accounts2.getAccounts().get(0).getEmail());
+      System.out.println(((User) accounts2.getAccounts().get(0)).getSocialNumber());
+      System.out.println(accounts2.getAccounts().get(1).getPassword());
+    } catch (IllegalStateException | IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    
+
+  }
 }
