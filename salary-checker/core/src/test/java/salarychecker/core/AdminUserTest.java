aaf1d985a472672582f7a6b398a3700a1a10d5d9
jakobke <jakobke@stud.ntnu.no>
2021-10-13 13:57:10 +0200

    (#40) Fixed an error in AdminUserTest

--- salary-checker/core/src/test/java/salarychecker/core/AdminUserTest.java
+++ salary-checker/core/src/test/java/salarychecker/core/AdminUserTest.java
@@ -48,6 +48,7 @@ public class AdminUserTest{
 
     @Test
     public void testCreateUser(){
+        adminUser.setAccounts(accounts);
         adminUser.createUser("Jens", "Jensen", "jensen@salarychecker.com", "Jensen123!", "31129969420", 66638, "sjef@salarychecker.com", 22.0);
         Assertions.assertNotNull(accounts.getUser("jensen@salarychecker.com", "Jensen123!"));
     }
