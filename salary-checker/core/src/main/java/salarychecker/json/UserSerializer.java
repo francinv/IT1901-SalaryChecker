9bd6cefdf60bb518da8fe1354f0e349a682b729a
francinv <francinv@stud.ntnu.no>
2021-10-14 16:31:59 +0200

    (#36) Added encryption in seralizer

--- salary-checker/core/src/main/java/salarychecker/json/UserSerializer.java
+++ salary-checker/core/src/main/java/salarychecker/json/UserSerializer.java
@@ -1,11 +1,19 @@
 package salarychecker.json;
 
 import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
+import salarychecker.core.EncryptDecrypt;
 import salarychecker.core.User;
 
 public class UserSerializer extends JsonSerializer<User> {
@@ -23,6 +31,7 @@ format:
     "taxCount": "...",
 }
 */
+    EncryptDecrypt encryptDecrypt = new EncryptDecrypt();
 
     @Override
     public void serialize(User user, JsonGenerator jsonGen, SerializerProvider serializerProvider)
@@ -33,8 +42,18 @@ format:
         jsonGen.writeStringField("firstname", user.getFirstname());
         jsonGen.writeStringField("lastname", user.getLastname());
         jsonGen.writeStringField("email", user.getEmail());
-        jsonGen.writeStringField("password", user.getPassword());
-        jsonGen.writeStringField("socialNumber", user.getSocialNumber());
+        try {
+            jsonGen.writeStringField("password", encryptDecrypt.encrypt(user.getPassword(), user.getFirstname()+user.getLastname()));
+        } catch (InvalidKeyException | NoSuchPaddingException | NoSuchAlgorithmException
+                | InvalidAlgorithmParameterException | BadPaddingException | IllegalBlockSizeException e1) {
+            e1.printStackTrace();
+        }
+        try {
+            jsonGen.writeStringField("socialNumber", encryptDecrypt.encrypt(user.getSocialNumber(), user.getLastname()+user.getFirstname()));
+        } catch (InvalidKeyException | NoSuchPaddingException | NoSuchAlgorithmException
+                | InvalidAlgorithmParameterException | BadPaddingException | IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
         jsonGen.writeNumberField("employeeNumber", user.getEmployeeNumber());
         jsonGen.writeStringField("employerEmail", user.getEmployerEmail());
 
