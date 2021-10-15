4fc41b72385cda138528b374982ffc7726774166
Seran Shanmugathas <Seran@live.no>
2021-10-15 13:52:41 +0200

    (#24) UserSerializer now writes UserSale

--- salary-checker/core/src/main/java/salarychecker/json/UserSerializer.java
+++ salary-checker/core/src/main/java/salarychecker/json/UserSerializer.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
 import salarychecker.core.User;
+import salarychecker.core.UserSale;
 
 public class UserSerializer extends JsonSerializer<User> {
 
@@ -21,22 +22,32 @@ format:
     "employeeNyumber": "...",
     "employerEmail": "...",
     "taxCount": "...",
+    "userSale": [...]
 }
 */
 
     @Override
     public void serialize(User user, JsonGenerator jsonGen, SerializerProvider serializerProvider)
         throws IOException {
-    jsonGen.writeStartObject();
+        jsonGen.writeStartObject();
 
-    jsonGen.writeStringField("firstname", user.getFirstname());
-    jsonGen.writeStringField("lastname", user.getLastname());
-    jsonGen.writeStringField("email", user.getEmail());
-    jsonGen.writeStringField("password", user.getPassword());
-    jsonGen.writeStringField("socialNumber", user.getSocialNumber());
-    jsonGen.writeNumberField("employeeNumber", user.getEmployeeNumber());
-    jsonGen.writeStringField("employerEmail", user.getEmployerEmail());
+        jsonGen.writeStringField("firstname", user.getFirstname());
+        jsonGen.writeStringField("lastname", user.getLastname());
+        jsonGen.writeStringField("email", user.getEmail());
+        jsonGen.writeStringField("password", user.getPassword());
+        jsonGen.writeStringField("socialNumber", user.getSocialNumber());
+        jsonGen.writeNumberField("employeeNumber", user.getEmployeeNumber());
+        jsonGen.writeStringField("employerEmail", user.getEmployerEmail());
+        jsonGen.writeNumberField("taxCount", user.getTaxCount());
+        
+        if (user.getUserSaleList() != null) {
+            jsonGen.writeArrayFieldStart("userSale");
+                for (UserSale userSale : user.getUserSaleList()) {
+                    jsonGen.writeObject(userSale);
+                }
+            jsonGen.writeEndArray();
+        }
 
-    jsonGen.writeEndObject();
+        jsonGen.writeEndObject();
     }
 }
