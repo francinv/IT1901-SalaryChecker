003a52de40af32e82311584a44548a12de11575f
Seran Shanmugathas <Seran@live.no>
2021-10-20 17:19:21 +0200

    (#37) Updated test

--- salary-checker/core/src/test/java/salarychecker/json/SalaryCheckerPersistenceTest.java
+++ salary-checker/core/src/test/java/salarychecker/json/SalaryCheckerPersistenceTest.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -27,17 +29,16 @@ public class SalaryCheckerPersistenceTest {
     User hammad = new User("Hammad","Siddiqui","ham@mad.no","Qwerty!123", "23020094332", 12345, "employer@gmail.com", 30, 152.50);
     AdminUser francin = new AdminUser("Francin", "Vincent", "francin@vincent.no", "Vandre!123");
     
-    UserSale userSale = new UserSale("Januar 21", 0, 0);
+    UserSale userSale = new UserSale("Januar 2021", 15000, 10000);
     userSale.setDifference();
-    hammad.getUserSaleList().add(userSale);
+    hammad.addUserSale(userSale);
     accounts.addUser(hammad);
     accounts.addUser(francin);
 
     try {
-      StringWriter writer = new StringWriter();
-      SCP.writeAccounts(accounts, writer);
-      String json = writer.toString();
-      Accounts accounts2 = SCP.readAccounts(new StringReader(json));
+      SCP.setSaveFile("AccountsTest.json");
+      SCP.saveAccounts(accounts);
+      Accounts accounts2 = SCP.loadAccounts();
       assertTrue(accounts2.iterator().hasNext());
       User hammad2 = ((User)accounts2.iterator().next());
       assertNotEquals("Brage", hammad2.getFirstname());
@@ -47,11 +48,11 @@ public class SalaryCheckerPersistenceTest {
       assertTrue(it.hasNext());
       SalaryCheckerModuleTest.checkAdminUser((AdminUser) it.next(), "Francin", "Vincent", "francin@vincent.no");
       assertFalse(it.hasNext());
-      assertTrue(hammad2.getUserSaleList().size() == 0);
-      assertThrows(IndexOutOfBoundsException.class, () -> hammad2.getUserSaleList().get(0).getDifference());
-      assertThrows(IndexOutOfBoundsException.class, () -> hammad2.getUserSaleList().get(0).getPaid());
-      assertThrows(IndexOutOfBoundsException.class, () -> hammad2.getUserSaleList().get(0).getExpected());
-      assertThrows(IndexOutOfBoundsException.class, () -> hammad2.getUserSaleList().get(0).getSalesperiod());
+      assertTrue(hammad2.getUserSaleList().size() == 1);
+      assertTrue(hammad2.getUserSaleList().get(0).getDifference() == 5000);
+      assertTrue(hammad2.getUserSaleList().get(0).getPaid() == 10000);
+      assertTrue(hammad2.getUserSaleList().get(0).getExpected() == 15000);
+      assertEquals("Januar 2021", hammad2.getUserSaleList().get(0).getSalesperiod());
       
     } catch (IOException e) {
       fail();
