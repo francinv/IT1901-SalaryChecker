382b66a77674b222a402a9f962cf7a88619fcf02
Seran Shanmugathas <Seran@live.no>
2021-10-22 01:10:01 +0200

    (#69) Added javadoc

--- salary-checker/core/src/main/java/salarychecker/core/AbstractUser.java
+++ salary-checker/core/src/main/java/salarychecker/core/AbstractUser.java
@@ -3,6 +3,10 @@ package salarychecker.core;
 import java.util.ArrayList;
 import java.util.Collection;
 
+/**
+ * Abstract class that containts the mutual implementation for 
+ * User and AdminUser
+ */
 public abstract class AbstractUser {
 
     protected String firstname;
@@ -10,14 +14,22 @@ public abstract class AbstractUser {
     protected String email;
     protected String password;
 
-    
-
     protected UserValidation userValidation = new UserValidation();
     protected Collection<IUserObserver> userObs = new ArrayList<IUserObserver>();
 
+    /**
+     * Access method for firstname
+     * 
+     * @return the firstname
+     */
     public String getFirstname() {
         return firstname;
     }
+
+    /**
+     * Mutation method for new firstname
+     * @param firstname the firstname to set
+     */
     public void setFirstname(String firstname) {
         userValidation.checkValidFirstname(firstname);
         this.firstname = firstname;
@@ -25,9 +37,20 @@ public abstract class AbstractUser {
             IUserObserver.userInfoStringChanged((User) this, firstname);
         }
     }
+
+    /**
+     * Access method for lastname
+     * 
+     * @return the lastname
+     */
     public String getLastname() {
         return lastname;
     }
+
+    /**
+     * Mutation method for new lastname
+     * @param lastname the firstname to set
+     */
     public void setLastname(String lastname) {
         userValidation.checkValidLastname(lastname);
         this.lastname = lastname;
@@ -35,9 +58,20 @@ public abstract class AbstractUser {
             IUserObserver.userInfoStringChanged((User) this, lastname);
         }
     }
+
+    /**
+     * Access method for email
+     * 
+     * @return the email
+     */
     public String getEmail() {
         return email;
     }
+
+    /**
+     * Mutation method for new email
+     * @param email the firstname to set
+     */
     public void setEmail(String email) {
         userValidation.checkValidEmail(email);
         this.email = email;
@@ -45,9 +79,20 @@ public abstract class AbstractUser {
             IUserObserver.userInfoStringChanged((User) this, email);
         }
     }
+
+    /**
+     * Access method for password
+     * 
+     * @return the password
+     */
     public String getPassword() {
         return password;
     }
+
+    /**
+     * Mutation method for new password
+     * @param password the firstname to set
+     */
     public void setPassword(String password) {
         userValidation.checkValidPassword(password);
         this.password = password;
@@ -56,16 +101,21 @@ public abstract class AbstractUser {
         }
     }
 
-
+    /**
+     * Method to add a new observer of this class
+     * @param uObserver the observer
+     */
     public void addObserver(IUserObserver uObserver){
         userObs.add(uObserver);
     }
 
+    /**
+     * Removes a observer of this class
+     * @param uObserver the observer to remove
+     */
     public void removeObserver(IUserObserver uObserver){
         userObs.remove(uObserver);
     }
-
-
 
     @Override
     public String toString() {
@@ -76,7 +126,4 @@ public abstract class AbstractUser {
             ", password='" + getPassword() + "'" +
             "}";
     }
-
-    
-
 }
