43ff42cf829965ca3f59705a55de9f8cbccbba11
Seran Shanmugathas <Seran@live.no>
2021-10-22 01:39:18 +0200

    (#69) Removed unused imports

--- salary-checker/core/src/test/java/salarychecker/core/CalculationTest.java
+++ salary-checker/core/src/test/java/salarychecker/core/CalculationTest.java
@@ -1,11 +1,9 @@
 package salarychecker.core;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
