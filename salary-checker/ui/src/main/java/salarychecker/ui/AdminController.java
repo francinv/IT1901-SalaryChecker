ada1c1ad8e2ba4e01d3961dfc6bfe6e022e9036a
Seran Shanmugathas <Seran@live.no>
2021-10-22 14:09:01 +0200

    (#69) Removed unused imports

--- salary-checker/ui/src/main/java/salarychecker/ui/AdminController.java
+++ salary-checker/ui/src/main/java/salarychecker/ui/AdminController.java
@@ -16,7 +16,6 @@ import salarychecker.core.*;
 import salarychecker.json.SalaryCheckerPersistence;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
