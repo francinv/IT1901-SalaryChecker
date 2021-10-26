f77302d33990657d80a0de92c06c0df8f382c357
Seran Shanmugathas <Seran@live.no>
2021-10-26 16:58:26 +0200

    (#72) Fixed CheckStyle in Accounts

--- salary-checker/core/src/main/java/salarychecker/core/Accounts.java
+++ salary-checker/core/src/main/java/salarychecker/core/Accounts.java
@@ -6,206 +6,301 @@ import java.util.List;
 
 /**
  * Class which contains a list of all registred Users.
- * This class implements IUserObserver, this is needed to 
+ * This class implements IUserObserver, this is needed to
  * observe changes in User object
  */
 public class Accounts implements IUserObserver {
 
-    private List<AbstractUser> accounts = new ArrayList<>();
+  private final List<AbstractUser> accounts = new ArrayList<>();
 
-    /**
-     * Access method for accounts
-     * 
-     * @return the accounts
-     */
-    public List<AbstractUser> getAccounts() {
-        return new ArrayList<>(accounts);
+  /**
+   * Access method for accounts.
+   *
+   * @return the accounts
+   */
+  public List<AbstractUser> getAccounts() {
+    return new ArrayList<>(accounts);
+  }
+
+  /**
+   * Adds new User to this list of Users.
+   *
+   * @param user the user to add
+   * @throws IllegalStateException if the user already exists.
+   */
+  public void addUser(AbstractUser user) {
+    if (contains(user)) {
+      throw new IllegalArgumentException("User already exists!");
     }
+    this.accounts.add(user);
+  }
 
-    /**
-     * Adds new User to this list of Users.
-     *
-     * @param user the user to add
-     * @throws IllegalStateException if the user already exists.
-     */
-    public void addUser(AbstractUser user) {
-        if (contains(user)) {
-            throw new IllegalArgumentException("User already exists!");
-        }
-        this.accounts.add(user);
+  /**
+   * Removes already existing User.
+   *
+   * @param user the user to remove
+   * @throws IllegalStateException if the user dosen't already exists.
+   */
+  public void removeUser(AbstractUser user) {
+    if (!contains(user)) {
+      throw new IllegalArgumentException("User does not exists!");
     }
+    this.accounts.remove(user);
+  }
 
-    /**
-     * Removes already existing User.
-     *
-     * @param user the user to remove
-     * @throws IllegalStateException if the user dosen't already exists.
-     */
-    public void removeUser(AbstractUser user) {
-        if (!contains(user)) {
-            throw new IllegalArgumentException("User does not exists!");
-        }
-        this.accounts.remove(user);
+  /**
+   * Iterator to easilly move between objects in list.
+   *
+   * @return iterator of accounts
+   */
+  public Iterator<AbstractUser> iterator() {
+    return accounts.iterator();
+  }
+
+  /**
+   * Finds the index of the user in list.
+   *
+   * @param user to find index for
+   */
+  public int indexOf(AbstractUser user) {
+    return accounts.indexOf(user);
+  }
+
+  /**
+   * Checks if the given user exists in the list of users.
+   *
+   * @param user to check
+   * @return true or false based on if the user exists
+   */
+  public boolean contains(AbstractUser user) {
+    return accounts.stream().anyMatch(u -> u.getEmail().equals(user.getEmail()));
+  }
+
+  /**
+   * Checks if the user login is valid.
+   *
+   * @param email email to check
+   * @param password password to check
+   * @return boolean
+   */
+  public boolean checkValidUserLogin(String email, String password) {
+    AbstractUser user = null;
+
+    for (AbstractUser ab : accounts) {
+      if (ab.getEmail().equals(email)) {
+        user = ab;
+      }
     }
+    return user != null && user.getPassword().equals(password);
+  }
 
-    /**
-     * Iterator to easilly move between objects in list
-     * @return iterator of accounts 
-     */
-    public Iterator<AbstractUser> iterator() {
-        return accounts.iterator();
+  /**
+   * Gets the user by email and password.
+   *
+   * @param email the email
+   * @param password the password
+   * @return the user
+   */
+  public AbstractUser getUser(String email, String password) {
+    AbstractUser user = null;
+
+    for (AbstractUser ab : accounts) {
+      if (ab.getEmail().equals(email)) {
+        user = ab instanceof User ? (User) ab : (AdminUser) ab;
+      }
     }
-
-    /**
-     * Finds the index of the user in list
-     * @param user to find index for
-     */
-    public int indexOf(AbstractUser user) {
-        return accounts.indexOf(user);
+    if (user == null) {
+      return null;
     }
-
-    /**
-     * Checks if the given user exists in the list of users.
-     * @param user to check
-     * @return true or false based on if the user exists 
-     */
-    public boolean contains(AbstractUser user) {
-        return accounts.stream().anyMatch(u -> u.getEmail().equals(user.getEmail()));
+    if (user.getPassword().equals(password)) {
+      return user;
     }
+    return null;
+  }
 
-    /**
-     * Checks if the user login is valid
-     * @param email
-     * @param password
-     * @return boolean
-     */
-    public boolean checkValidUserLogin(String email, String password) {
-        AbstractUser user = null;
+  /**
+   * Updates password of a specific user.
+   *
+   * @param user the user
+   * @param password new password
+   */
+  public void updatePassword(User user, String password) {
+    accounts.stream().filter(u -> u.getEmail().equals(user.getEmail()))
+                     .findAny()
+                     .ifPresent(u -> u.setPassword(password));
+  }
 
-        for (AbstractUser ab : accounts) {
-            if (ab.getEmail().equals(email)) {
-                user = ab;
-            }
-        }
-        return user.getPassword().equals(password);
+  /**
+   * Updates email of a specific user.
+   *
+   * @param user the user
+   * @param email new email
+   */
+  public void updateEmail(User user, String email) {
+    accounts.stream().filter(u -> u.getEmail().equals(user.getEmail()))
+                     .findAny()
+                     .ifPresent(u -> u.setEmail(email));
+  }
+
+  /**
+   * Updates firstname of a specific user.
+   *
+   * @param user the user
+   * @param firstname the firstname
+   */
+  public void updateFirstname(User user, String firstname) {
+    accounts.stream().filter(u -> u.getEmail().equals(user.getEmail()))
+                     .findAny()
+                     .ifPresent(u -> u.setFirstname(firstname));
+  }
+
+  /**
+   * Updates lastname of a specific user.
+   *
+   * @param user the user
+   * @param lastname new lastname
+   */
+  public void updateLastname(User user, String lastname) {
+    accounts.stream().filter(u -> u.getEmail().equals(user.getEmail()))
+                     .findAny()
+                     .ifPresent(u -> u.setLastname(lastname));
+  }
+
+  /**
+   * Updates employer email of a specific user.
+   *
+   * @param user the user
+   * @param employermail the employermail
+   */
+  public void updateEmployerEmail(User user, String employermail) {
+    for (AbstractUser u : accounts) {
+      if (u.getEmail().equals(user.getEmail())) {
+        user = (User) u;
+      }
     }
+    user.setEmployerEmail(employermail);
+  }
 
-
-    public AbstractUser getUser(String email, String password) {
-        AbstractUser user = null;
-
-        for (AbstractUser ab : accounts) {
-            if (ab.getEmail().equals(email)) {
-                user = ab instanceof User ? (User) ab : (AdminUser) ab;
-            } 
-        }
-        if (user == null) {
-            return null;
-        }
-        if (user.getPassword().equals(password)) {
-            return user;
-        }
-        return null;
+  /**
+   * Updates houre rate of a specific user.
+   *
+   * @param user the user
+   * @param hoursal new hoursal
+   */
+  public void updateHourSal(User user, Double hoursal) {
+    for (AbstractUser u : accounts) {
+      if (u.getEmail().equals(user.getEmail())) {
+        user = (User) u;
+      }
     }
+    user.setHourRate(hoursal);
+  }
 
-    public void updatePassword(User user, String password) {
-        accounts.stream().filter(u->u.getEmail().equals(user.getEmail())).findAny().ifPresent(u->u.setPassword(password));
+  /**
+   * Updates tax count of a specific user.
+   *
+   * @param user the user
+   * @param taxcount new tax count
+   */
+  public void updateTaxCount(User user, Double taxcount) {
+    for (AbstractUser u : accounts) {
+      if (u.getEmail().equals(user.getEmail())) {
+        user = (User) u;
+      }
     }
+    user.setHourRate(taxcount);
+  }
 
-    public void updateEmail(User user, String email) {
-        accounts.stream().filter(u->u.getEmail().equals(user.getEmail())).findAny().ifPresent(u->u.setEmail(email));
+  /**
+   * Updates employee number of a specific user.
+   *
+   * @param user the user
+   * @param employeenumber new employee number
+   */
+  public void updateEmployeeNumber(User user, int employeenumber) {
+    for (AbstractUser u : accounts) {
+      if (u.getEmail().equals(user.getEmail())) {
+        user = (User) u;
+      }
     }
+    user.setHourRate(employeenumber);
+  }
 
-    public void updateFirstname(User user, String firstname) {
-        accounts.stream().filter(u->u.getEmail().equals(user.getEmail())).findAny().ifPresent(u->u.setFirstname(firstname));
+  /**
+   * Adds usersale to the user.
+   *
+   * @param user the user
+   * @param usale the user sale
+   */
+  public void addUserSale(User user, UserSale usale) {
+    accounts.stream().filter(u -> u.getEmail().equals(user.getEmail()))
+                     .findAny()
+                     .ifPresent(u -> ((User) u).addUserSale(usale));
+  }
+
+  /**
+   * This method is used to inform the observers.
+   *
+   * @param user the user
+   * @param userSale the usersale
+   */
+  public void usersaleAdded(User user, UserSale userSale) {
+    this.addUserSale(user, userSale);
+  }
+
+  /**
+   * This method is used to inform the observers.
+   * The method checks wether the double is a tax count or hour rate,
+   * and than calls the method meant for each of the possibilities.
+   *
+   * @param user the user
+   * @param changeddouble the changed double
+   */
+  public void userInfoDoubleChanged(User user, Double changeddouble) {
+    if (changeddouble.equals(user.getTaxCount())) {
+      updateTaxCount(user, changeddouble);
+    } else if (changeddouble.equals(user.getHourRate())) {
+      updateHourSal(user, changeddouble);
     }
+  }
 
-    public void updateLastname(User user, String lastname) {
-        accounts.stream().filter(u->u.getEmail().equals(user.getEmail())).findAny().ifPresent(u->u.setLastname(lastname));
+  /**
+   * This method is used to inform the observers.
+   *
+   * @param user the user
+   * @param changedTaxCount the changed tax count
+   */
+  public void userInfoTaxCountChanged(User user, Integer changedTaxCount) {
+    if (changedTaxCount.equals(user.getEmployeeNumber())) {
+      updateEmployeeNumber(user, changedTaxCount);
     }
+  }
 
-    public void updateEmployerEmail(User user, String employermail){
-        for (AbstractUser u : accounts){
-            if(u.getEmail().equals(user.getEmail())){
-                user = (User) u;
-            }
-        }
-        user.setEmployerEmail(employermail);
+  /**
+   * This method is used to inform the observers.
+   * The method checks wether the string is a email, employer email,
+   * password, firstname, or lastname, and than calls the method
+   * meant for each of the possibilities.
+   *
+   * @param user the user
+   * @param changedstring the changed string
+   */
+  public void userInfoStringChanged(User user, String changedstring) {
+    if (changedstring.equals(user.getEmail())) {
+      updateEmail(user, changedstring);
+    } else if (changedstring.equals(user.getEmployerEmail())) {
+      updateEmployerEmail(user, changedstring);
+    } else if (changedstring.equals(user.getPassword())) {
+      updatePassword(user, changedstring);
+    } else if (changedstring.equals(user.getFirstname())) {
+      updateFirstname(user, changedstring);
+    } else if (changedstring.equals(user.getLastname())) {
+      updateLastname(user, changedstring);
     }
+  }
 
-    public void updateHourSal(User user, Double hoursal){
-        for (AbstractUser u: accounts){
-            if(u.getEmail().equals(user.getEmail())){
-                user = (User) u;
-            }
-        }
-        user.setHourRate(hoursal);
-    }
-
-    public void updateTaxCount(User user, Double taxcount){
-        for (AbstractUser u: accounts){
-            if(u.getEmail().equals(user.getEmail())){
-                user = (User) u;
-            }
-        }
-        user.setHourRate(taxcount);
-    }
-
-    public void updateEmployeeNumber(User user, int employeenumber){
-        for (AbstractUser u: accounts){
-            if(u.getEmail().equals(user.getEmail())){
-                user = (User) u;
-            }
-        }
-        user.setHourRate(employeenumber);
-    }
-
-    public void addUserSale(User user, UserSale usale){
-        accounts.stream().filter(u->u.getEmail().equals(user.getEmail())).findAny().ifPresent(u->((User) u).addUserSale(usale));
-    }
-
-    public void usersaleAdded(User user, UserSale uSale) {
-        this.addUserSale(user, uSale);
-    }
-
-    public void userInfoDoubleChanged(User user, Double changeddouble) {
-        if (changeddouble.equals(user.getTaxCount())){
-            updateTaxCount(user, changeddouble);
-        }
-        else if (changeddouble.equals(user.getHourRate())){
-            updateHourSal(user, changeddouble);
-        }
-
-    }
-
-    public void userInfoIntChanged(User user, Integer changedint) {
-        if(changedint.equals(user.getEmployeeNumber())){
-            updateEmployeeNumber(user, changedint);
-        }
-    }
-
-    public void userInfoStringChanged(User user, String changedstring) {
-        if(changedstring.equals(user.getEmail())){
-            updateEmail(user, changedstring);
-        }
-        else if (changedstring.equals(user.getEmployerEmail())){
-            updateEmployerEmail(user, changedstring);
-        }
-        else if (changedstring.equals(user.getPassword())){
-            updatePassword(user, changedstring);
-        }
-        else if (changedstring.equals(user.getFirstname())){
-            updateFirstname(user, changedstring);
-        }
-        else if (changedstring.equals(user.getLastname())) {
-            updateLastname(user, changedstring);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-            " accounts='" + getAccounts() + "'" +
-            "}";
-    }
+  @Override
+  public String toString() {
+    return "{ accounts='" + getAccounts() + "'}";
+  }
 }
