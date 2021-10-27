5445021086cff589657bd86eaf1101f2b139b713
Seran Shanmugathas <Seran@live.no>
2021-10-27 11:59:35 +0200

    (#72) Fixed CheckStyle in AdminUserDeserializer

--- salary-checker/json/src/main/java/salarychecker/json/internal/AdminUserDeserializer.java
+++ salary-checker/json/src/main/java/salarychecker/json/internal/AdminUserDeserializer.java
@@ -12,60 +12,61 @@ import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-
-import salarychecker.core.EncryptDecrypt;
 import salarychecker.core.AdminUser;
+import salarychecker.core.EncryptDecrypt;
 
+/**
+ * Class to Deserialize {@link AdminUser}.
+ */
 public class AdminUserDeserializer extends JsonDeserializer<AdminUser> {
 
-    private EncryptDecrypt encryptDecrypt = new EncryptDecrypt();
+  private final EncryptDecrypt encryptDecrypt = new EncryptDecrypt();
 
-    @Override
-    public AdminUser deserialize(JsonParser parser, DeserializationContext ctxt)
-        throws IOException, JsonProcessingException {
+  @Override
+  public AdminUser deserialize(JsonParser parser, DeserializationContext ctxt)
+      throws IOException, JsonProcessingException {
 
-        TreeNode treeNode = parser.getCodec().readTree(parser);
-        return deserialize((JsonNode) treeNode);
-    }
+    TreeNode treeNode = parser.getCodec().readTree(parser);
+    return deserialize((JsonNode) treeNode);
+  }
 
-    AdminUser deserialize(JsonNode jsonNode) {
+  private AdminUser deserialize(JsonNode jsonNode) {
     if (jsonNode instanceof ObjectNode objectNode) {
-        AdminUser user = new AdminUser();
-        JsonNode firstnameNode = objectNode.get("firstname");
+      AdminUser user = new AdminUser();
+      JsonNode firstnameNode = objectNode.get("firstname");
 
-        if (firstnameNode instanceof TextNode) {
-            user.setFirstname(firstnameNode.asText());
+      if (firstnameNode instanceof TextNode) {
+        user.setFirstname(firstnameNode.asText());
+      }
+
+      JsonNode lastnameNode = objectNode.get("lastname");
+      if (lastnameNode instanceof TextNode) {
+        user.setLastname(lastnameNode.asText());
+      }
+
+      JsonNode emailNode = objectNode.get("email");
+      if (emailNode instanceof TextNode) {
+        user.setEmail(emailNode.asText());
+      }
+
+      JsonNode passwordNode = objectNode.get("password");
+      if (passwordNode instanceof TextNode) {
+
+        try {
+          String decryptedPassword = encryptDecrypt.decrypt(passwordNode.asText(), firstnameNode.asText()
+              + lastnameNode.asText());
+          user.setPassword(decryptedPassword);
+        } catch (InvalidKeyException | NoSuchPaddingException | NoSuchAlgorithmException
+                | InvalidAlgorithmParameterException | BadPaddingException
+                | IllegalBlockSizeException e) {
+          e.printStackTrace();
         }
-
-        JsonNode lastnameNode = objectNode.get("lastname");
-        if (lastnameNode instanceof TextNode) {
-            user.setLastname(lastnameNode.asText());
-        }
-
-        JsonNode emailNode = objectNode.get("email");
-        if (emailNode instanceof TextNode) {
-            user.setEmail(emailNode.asText());
-        }
-
-        JsonNode passwordNode = objectNode.get("password");
-        if (passwordNode instanceof TextNode) {
-
-            try {
-                String decryptedPassword = encryptDecrypt.decrypt(passwordNode.asText(), firstnameNode.asText() + lastnameNode.asText());
-                user.setPassword(decryptedPassword);
-            } catch (InvalidKeyException | NoSuchPaddingException | NoSuchAlgorithmException
-                    | InvalidAlgorithmParameterException | BadPaddingException | IllegalBlockSizeException e) {
-                e.printStackTrace();
-            }
-    
-        }
-
-        return user;
+      }
+      return user;
     }
-        return null;
-    }   
+    return null;
+  }
 }
