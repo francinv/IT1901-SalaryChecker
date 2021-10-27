a2febda5b6ae9da793217b6bcfef04ad76758f33
Seran Shanmugathas <Seran@live.no>
2021-10-27 12:09:59 +0200

    (#72) Fixed CheckStyle in SalaryCheckerModule

--- salary-checker/json/src/main/java/salarychecker/json/internal/SalaryCheckerModule.java
+++ salary-checker/json/src/main/java/salarychecker/json/internal/SalaryCheckerModule.java
@@ -2,26 +2,27 @@ package salarychecker.json.internal;
 
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import salarychecker.core.*;
+import salarychecker.core.Accounts;
+import salarychecker.core.AdminUser;
+import salarychecker.core.User;
 
 /**
  * A Jackson module for configuring JSON serialization of User instances.
  */
 public class SalaryCheckerModule extends SimpleModule {
 
-    private static final String NAME = "UserModule";
+  private static final String NAME = "UserModule";
 
-    /**
-     * Initializes this SalaryCheckerModule with appropriate serializers and deserializers.
-     */
-    public SalaryCheckerModule() {
-        super(NAME, Version.unknownVersion());
-        addSerializer(User.class, new UserSerializer());
-        addSerializer(AdminUser.class, new AdminUserSerializer());
-        addSerializer(Accounts.class, new AccountsSerializer());
-        addDeserializer(User.class, new UserDeserializer());
-        addDeserializer(AdminUser.class, new AdminUserDeserializer());
-        addDeserializer(Accounts.class, new AccountsDeserializer());
-    }
-    
+  /**
+   * Initializes this SalaryCheckerModule with appropriate serializers and deserializers.
+   */
+  public SalaryCheckerModule() {
+    super(NAME, Version.unknownVersion());
+    addSerializer(User.class, new UserSerializer());
+    addSerializer(AdminUser.class, new AdminUserSerializer());
+    addSerializer(Accounts.class, new AccountsSerializer());
+    addDeserializer(User.class, new UserDeserializer());
+    addDeserializer(AdminUser.class, new AdminUserDeserializer());
+    addDeserializer(Accounts.class, new AccountsDeserializer());
+  }
 }
