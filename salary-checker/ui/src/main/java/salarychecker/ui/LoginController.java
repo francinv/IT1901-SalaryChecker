69b5c925eef2e2d4b50c524aaef5b82222ad8a75
Seran Shanmugathas <Seran@live.no>
2021-10-27 13:24:47 +0200

    (#72) Fixed Checkstyle LoginController

--- salary-checker/ui/src/main/java/salarychecker/ui/LoginController.java
+++ salary-checker/ui/src/main/java/salarychecker/ui/LoginController.java
@@ -1,5 +1,6 @@
 package salarychecker.ui;
 
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -18,105 +19,102 @@ import salarychecker.core.User;
 import salarychecker.core.UserValidation;
 import salarychecker.json.SalaryCheckerPersistence;
 
-import java.io.IOException;
-
+/**
+ * Controller class for the Login-scene.
+ */
 public class LoginController {
 
-    @FXML private TextField email;
-    @FXML private TextField password;
-    @FXML private Button createButton;
-    @FXML private Text errorDisplay;
+  @FXML private TextField email;
+  @FXML private TextField password;
+  @FXML private Button createButton;
+  @FXML private Text errorDisplay;
 
-    private AbstractUser user;
-    private UserValidation userval = new UserValidation();
-    private SalaryCheckerPersistence persistence = new SalaryCheckerPersistence();
+  private AbstractUser user;
+  private final UserValidation userval = new UserValidation();
+  private final SalaryCheckerPersistence persistence = new SalaryCheckerPersistence();
 
-    @FXML
-    void initialize(){
-        persistence.setSaveFile("Accounts.json");
+  @FXML
+  void initialize() {
+    persistence.setSaveFile("Accounts.json");
+  }
+
+  @FXML
+  void userLogIn(ActionEvent event) throws IOException {
+    String usernameField = email.getText();
+    String passwordField = password.getText();
+    Accounts accounts;
+    accounts = persistence.loadAccounts();
+
+    try {
+      userval.checkValidEmail(usernameField);
+      userval.checkValidPassword(passwordField);
+      userval.isNotExistingUser(usernameField, passwordField, accounts);
+      userval.isValidLogIn(usernameField, passwordField, accounts);
+      user = accounts.getUser(usernameField, passwordField);
+      if (user instanceof User) {
+        switchtoHomepageScene(event);
+      } else {
+        switchToAdminScene(event);
+      }
+    } catch (IllegalArgumentException e) {
+      errorDisplay.setText(e.getMessage());
     }
+  }
 
-    @FXML
-    void userLogIn(ActionEvent event) throws IOException {
-        String usernameField = email.getText();
-        String passwordField = password.getText();
-        Accounts accounts;
-        accounts = persistence.loadAccounts();
-
-        try {
-            userval.checkValidEmail(usernameField);
-            userval.checkValidPassword(passwordField);
-            userval.isNotExistingUser(usernameField, passwordField, accounts);
-            userval.isValidLogIn(usernameField, passwordField, accounts);
-            user = accounts.getUser(usernameField, passwordField);
-            if (user instanceof User) {
-                switchtoHomepageScene(event);
-            }
-            else {
-                switchToAdminScene(event);
-            }
-        }
-        catch (IllegalArgumentException e) {
-            errorDisplay.setText(e.getMessage());
-        }
+  private void switchToAdminScene(ActionEvent event) {
+    Accounts accounts;
+    try {
+      FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Admin.fxml"));
+      Parent root = fxmlLoader.load();
+      AdminController adminController = fxmlLoader.getController();
+      adminController.setAdminUser((AdminUser) user);
+      accounts = persistence.loadAccounts();
+      adminController.setAccounts(accounts);
+      adminController.loadInfo();
+      adminController.loadListView();
+      ((AdminUser) user).addObserver(accounts);
+      Scene homepageScene = new Scene(root);
+      Stage window = (Stage) (((Node) event.getSource()).getScene().getWindow());
+      window.setScene(homepageScene);
+      window.show();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    private void switchToAdminScene(ActionEvent event) {
-        Accounts accounts;
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Admin.fxml"));
-            Parent root = fxmlLoader.load();
-            AdminController adminController = fxmlLoader.getController();
-            adminController.setAdminUser((AdminUser) user);
-            accounts = persistence.loadAccounts();
-            adminController.setAccounts(accounts);
-            adminController.loadInfo();
-            adminController.loadListView();
-            ((AdminUser) user).addObserver(accounts);
-            Scene homepageScene = new Scene(root);
-            Stage window = (Stage) (((Node) event.getSource()).getScene().getWindow());
-            window.setScene(homepageScene);
-            window.show();
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
+  private void switchtoHomepageScene(ActionEvent event) {
+    Accounts accounts;
+    try {
+      FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("HomePage.fxml"));
+      Parent root = fxmlLoader.load();
+      HomepageController homepageController = fxmlLoader.getController();
+      homepageController.setUser((User) user);
+      accounts = persistence.loadAccounts();
+      homepageController.setAccounts(accounts);
+      homepageController.loadInfo();
+      ((User) user).addObserver(accounts);
+      Scene homepageScene = new Scene(root);
+      Stage window = (Stage) (((Node) event.getSource()).getScene().getWindow());
+      window.setScene(homepageScene);
+      window.show();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    private void switchtoHomepageScene(ActionEvent event) {
-        Accounts accounts;
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("HomePage.fxml"));
-            Parent root = fxmlLoader.load();
-            HomepageController homepageController = fxmlLoader.getController();
-            homepageController.setUser((User) user);
-            accounts = persistence.loadAccounts();
-            homepageController.setAccounts(accounts);
-            homepageController.loadInfo();
-            ((User) user).addObserver(accounts);
-            Scene homepageScene = new Scene(root);
-            Stage window = (Stage) (((Node) event.getSource()).getScene().getWindow());
-            window.setScene(homepageScene);
-            window.show();
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-    }
+  @FXML
+  private void createUsersAction(ActionEvent event) throws IOException {
+    User testuser1 = new User("Seran", "Shanmugathas", "seran@live.no",
+        "Password123!", "22030191349", 12345, "employeer1@gmail.com", 30.0, 130);
+    AdminUser testuser2 = new AdminUser("Francin", "Vincent", "francin.vinc@gmail.com",
+        "Vandre333!");
 
-    @FXML
-    private void createUsersAction(ActionEvent event) throws IOException {
-        User testuser1 = new User("Seran", "Shanmugathas", "seran@live.no", "Password123!", "22030191349", 12345, "employeer1@gmail.com", 30.0, 130);
-        AdminUser testuser2 = new AdminUser("Francin", "Vincent", "francin.vinc@gmail.com", "Vandre333!");
+    Accounts acc = new Accounts();
+    acc.addUser(testuser1);
+    acc.addUser(testuser2);
 
-        Accounts acc = new Accounts();
-        acc.addUser(testuser1);
-        acc.addUser(testuser2);
-
-        persistence.saveAccounts(acc);
-        createButton.setText("Test users created!");
-        createButton.setTextFill(Paint.valueOf("#008000"));
-
-    }
-
+    persistence.saveAccounts(acc);
+    createButton.setText("Test users created!");
+    createButton.setTextFill(Paint.valueOf("#008000"));
+  }
 }
