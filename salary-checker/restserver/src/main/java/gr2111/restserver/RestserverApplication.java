8c611ad371ac6345360809933806a2df34e38c7c
Seran Shanmugathas <Seran@live.no>
2021-10-29 15:00:48 +0200

    (#85) Added module for restserver

---
+++ salary-checker/restserver/src/main/java/gr2111/restserver/RestserverApplication.java
@@ -0,0 +1,13 @@
+package gr2111.restserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestserverApplication.class, args);
+	}
+
+}
