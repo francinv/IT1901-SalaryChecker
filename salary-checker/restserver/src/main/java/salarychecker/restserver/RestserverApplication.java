22afe185d37ef83eb5db5a58ce80a975525aaf19
Seran Shanmugathas <Seran@live.no>
2021-10-31 22:55:11 +0100

    (#91) Changed name for RestserverApplication

--- salary-checker/restserver/src/main/java/salarychecker/restserver/RestserverApplication.java
+++ salary-checker/restserver/src/main/java/salarychecker/restserver/RestserverApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestserverApplication {
+public class RestServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestserverApplication.class, args);
+		SpringApplication.run(RestServerApplication.class, args);
 	}
 
 }
