3347265fab99dc0d26fa873470c578d0e8e013db
Seran Shanmugathas <Seran@live.no>
2021-11-01 12:42:03 +0100

    (#91) Added service annotaion

--- salary-checker/restserver/src/main/java/salarychecker/restserver/SalaryCheckerService.java
+++ salary-checker/restserver/src/main/java/salarychecker/restserver/SalaryCheckerService.java
@@ -2,14 +2,19 @@ package salarychecker.restserver;
 
 import salarychecker.core.AbstractUser;
 import salarychecker.core.Accounts;
+import salarychecker.core.AdminUser;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SalaryCheckerService {
 
     private final Accounts accounts = new Accounts();
  
     public List<AbstractUser> getAccounts() {
+        accounts.addUser(new AdminUser("firstname", "lastname", "email@gmail.com", "Vandre33!"));
         return accounts.getAccounts();
     }
 
