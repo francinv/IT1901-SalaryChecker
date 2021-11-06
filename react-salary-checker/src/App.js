ef7ffaad464575a39a3dc7b3bb150a551b503713
francinv <francinv@stud.ntnu.no>
2021-11-07 00:40:28 +0100

    (#119) Completed ProfilePage

--- react-salary-checker/src/App.js
+++ react-salary-checker/src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 import CalculationPage from './pages/CalculationPage';
 import HomePage from './pages/HomePage';
 import Login from './pages/LoginPage';
+import ProfilePage from './pages/ProfilePage';
 import SalariesPage from './pages/SalariesPage';
 
 function App() {
-  const isLoggedIn = true;
+  const [isLoggedIn, setIsLoggedIn] = React.useState(true);
   return (
     <div className="App">
       {
         isLoggedIn 
-        ? <SalariesPage />
+        ? <ProfilePage isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
         : <Login />
       }
     </div>
