f1541a476ad115730b77a58fd04ebc74b6c1e7a5
francinv <francinv@stud.ntnu.no>
2021-11-06 19:02:32 +0100

    (#111) (#112) Finished AdminPage and HomePage, rendered based on type of user and if user is logged in. This will be updated when we are fixing globalstate

---
+++ react-salary-checker/src/components/usernav/index.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { AppBar, IconButton, Menu, MenuItem, Toolbar } from "@mui/material";
+import { Box } from "@mui/system";
+import AccountCircle from '@mui/icons-material/AccountCircle';
+
+
+const UserNav = ({NavButton, LogoButton}) => { 
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const isMenuOpen = Boolean(anchorEl);
+
+    const handleProfileMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+    const menuId = 'primary-search-account-menu';
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static" sx={{
+            bgcolor:'black',
+          }}>
+            <Toolbar>
+              <LogoButton
+                component="div"
+                sx={{ display: { sm: 'block' } }}
+              >
+                $alary Checker
+              </LogoButton>
+              <Box sx={{ flexGrow: 1 }} />
+              <Box sx={{ display: 'flex' }}>
+                <NavButton variant="outlined">
+                  Kalkuler lønning
+                </NavButton>
+                <NavButton variant="outlined" sx={{margin:'0.5rem', color:'white'}}>
+                  Mine lønninger
+                </NavButton>
+                <IconButton
+                  size="large"
+                  edge="end"
+                  aria-label="account of current user"
+                  aria-controls={menuId}
+                  aria-haspopup="true"
+                  onClick={handleProfileMenuOpen}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+              </Box>
+            </Toolbar>
+          </AppBar>
+          <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            id={menuId}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
+          >
+            <MenuItem onClick={handleMenuClose}>Profil</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Logg out </MenuItem>
+          </Menu>
+        </Box>
+    );
+}
+
+export default UserNav;
