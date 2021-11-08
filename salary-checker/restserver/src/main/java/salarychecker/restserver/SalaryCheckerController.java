342831b23ce809dff932fafd82e215853dd18120
francinv <francinv@stud.ntnu.no>
2021-11-08 19:19:13 +0100

    (#109) Moved react app into ui

--- salary-checker/restserver/src/main/java/salarychecker/restserver/SalaryCheckerController.java
+++ salary-checker/restserver/src/main/java/salarychecker/restserver/SalaryCheckerController.java
@@ -3,6 +3,7 @@ package salarychecker.restserver;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,6 +30,7 @@ public class SalaryCheckerController {
     this.salaryCheckerService = salaryCheckerService;
   }  
   
+  @CrossOrigin(origins = "http://localhost:3000")
   @GetMapping
   public List<AbstractUser> getAccounts() {
     return salaryCheckerService.getAccounts();
