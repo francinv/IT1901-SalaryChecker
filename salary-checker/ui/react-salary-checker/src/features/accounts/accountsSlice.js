b1366ed91424c299d51d6711e87d80f3b7468a0a
francinv <francinv@stud.ntnu.no>
2021-11-08 19:19:13 +0100

    (#109) Moved react app into ui

---
+++ salary-checker/ui/react-salary-checker/src/features/accounts/accountsSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const AccountsSlice = createSlice({
+    name: 'accounts',
+    initialState: {
+        accountsList: [],
+    },
+    reducers: {
+        addUser: (state, action) => {
+            state.accountsList.push(action.payload);
+        },
+        getUser: (state, action) => {
+            return state.accountsList.find(u => u.email === action.payload);
+        },
+        setAccounts: (state, action) => {
+            state.accountsList = action.payload;
+        },
+    },
+});
+
+export const {addUser, getUser, setAccounts} = AccountsSlice.actions;
+
+export const selectAccounts = (state) => state.accounts.accountsList;
+
+export default AccountsSlice.reducer;
