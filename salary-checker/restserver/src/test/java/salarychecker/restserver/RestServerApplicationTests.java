96dd65e298bbf20bc348081652b4e3518e54052d
Seran Shanmugathas <Seran@live.no>
2021-11-08 13:31:31 +0100

    (#104) Added springbootconfiguration

--- salary-checker/restserver/src/test/java/salarychecker/restserver/RestServerApplicationTests.java
+++ salary-checker/restserver/src/test/java/salarychecker/restserver/RestServerApplicationTests.java
@@ -1,9 +1,13 @@
 package salarychecker.restserver;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@SpringBootConfiguration
+@ContextConfiguration
 class RestServerApplicationTests {
 
 	@Test
