3348e0bbba9b6177e645482788b350179730fdc0
Seran Shanmugathas <Seran@live.no>
2021-11-12 13:11:35 +0100

    (#128) This controller now gets data from dataAccess class

--- salary-checker/ui/src/main/java/salarychecker/ui/LoginController.java
+++ salary-checker/ui/src/main/java/salarychecker/ui/LoginController.java
@@ -12,7 +12,6 @@ import salarychecker.core.Accounts;
 import salarychecker.core.AdminUser;
 import salarychecker.core.User;
 import salarychecker.core.UserValidation;
-import salarychecker.json.SalaryCheckerPersistence;
 
 /**
  * Controller class for the Login-scene.
@@ -27,17 +26,8 @@ public class LoginController extends AbstractController {
   private AbstractUser user = super.user;
   private Accounts accounts;
   private final UserValidation userval = new UserValidation();
-  private final SalaryCheckerPersistence persistence = new SalaryCheckerPersistence();
 
   private SalaryCheckerAccess dataAccess;
-
-  /**
-   * We use the initialize method to set saveFile for persistence.
-   */
-  @FXML
-  void initialize() {
-    persistence.setFilePath("Accounts.json");
-  }
 
   /**
    * This is the method that handles the log-in.
@@ -52,14 +42,14 @@ public class LoginController extends AbstractController {
   void userLogIn(ActionEvent event) throws IOException {
     String usernameField = email.getText();
     String passwordField = password.getText();
-    accounts = persistence.loadAccounts();
+    accounts = dataAccess.readAccounts();
 
     try {
       userval.checkValidEmail(usernameField);
       userval.checkValidPassword(passwordField);
       userval.isNotExistingUser(usernameField, passwordField, accounts);
       userval.isValidLogIn(usernameField, passwordField, accounts);
-      user = accounts.getUser(usernameField, passwordField);
+      user = dataAccess.userLogin(usernameField, passwordField);
       if (user instanceof User) {
         setScene(CONTROLLERS.HOME, event, user, accounts);
       } else {
@@ -79,16 +69,11 @@ public class LoginController extends AbstractController {
    */
   @FXML
   private void createUsersAction(ActionEvent event) throws IOException {
-    User testuser1 = new User("Seran", "Shanmugathas", "seran@live.no",
-        "Password123!", "22030191349", 12345, "employeer1@gmail.com", 30.0, 130);
-    AdminUser testuser2 = new AdminUser("Francin", "Vincent", "francin.vinc@gmail.com",
-        "Vandre333!");
+    dataAccess.createUser(new User("Seran", "Shanmugathas", "seran@live.no",
+        "Password123!", "22030191349", 12345, "employeer1@gmail.com", 30.0, 130));
+    dataAccess.createUser(new AdminUser("Francin", "Vincent", "francin.vinc@gmail.com",
+        "Vandre333!"));
 
-    Accounts acc = new Accounts();
-    acc.addUser(testuser1);
-    acc.addUser(testuser2);
-
-    persistence.saveAccounts(acc);
     createButton.setText("Test users created!");
     createButton.setTextFill(Paint.valueOf("#008000"));
   }
