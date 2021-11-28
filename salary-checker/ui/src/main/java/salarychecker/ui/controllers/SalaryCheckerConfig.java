9e296d41dfb49f239f7ce1e51374d6529a592ad5
Francin Anoj Vincent <francinv@stud.ntnu.no>
2021-11-28 13:36:19 +0100

    (#125) Created one it, tested that everything is working. Fixed some spotbugs. Trying to build app using mvn clean install will still fail.

--- salary-checker/ui/src/main/java/salarychecker/ui/controllers/SalaryCheckerConfig.java
+++ salary-checker/ui/src/main/java/salarychecker/ui/controllers/SalaryCheckerConfig.java
@@ -18,22 +18,12 @@ public class SalaryCheckerConfig {
    */
   public SalaryCheckerConfig() {
     this.properties = new Properties();
-
-        // if (isIT()) {
-        //     try (InputStream inputStream = new FileInputStream("src/test/resources/ajour/ui/salarychecker.properties")) {
-        //         properties.load(inputStream);
-        //     } catch (IOException e) {
-        //         e.printStackTrace();
-        //     }
-        // } else {
-            try (InputStream inputStream = 
-                    SalaryCheckerApp.class.getResourceAsStream("salarychecker.properties")) {
-                properties.load(inputStream);
-            } catch (IOException e) {
-                throw new IllegalStateException("Could not load salarychecker.properties");
-            }
-        // }
-    //}
+    try (InputStream inputStream =
+        SalaryCheckerApp.class.getResourceAsStream("salarychecker.properties")) {
+      properties.load(inputStream);
+    } catch (IOException e) {
+      throw new IllegalStateException("Could not load salarychecker.properties");
+    }
   }
 
     /**
@@ -47,16 +37,4 @@ public class SalaryCheckerConfig {
         return properties.getProperty(key);
     }
 
-    /**
-     * Checks with system properties if to run integration test. 
-     *
-     * @return a boolean wether to run IT.
-     */
-    private boolean isIT() {
-        try {
-            return System.getProperty("salarychecker.it").equals("true");
-        } catch (Exception e) {
-            return false;
-        }
-    }
 }
